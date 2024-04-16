ce9b8a7cb2575f6fb51f4042954820ef49046e53
Anatoly Ivakin <siggnull@gmail.com>
2024-04-16 20:45:20 +0300

    Add an application bar with a button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import './App.css'
+
 
 function App() {
   return (
-    <>
-      Hello World!
-    </>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Simple Pool
+          </Typography>
+          <Button variant="outlined" color="inherit">Connect Wallet</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   )
 }
 
