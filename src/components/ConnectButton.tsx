1c63de4d1ff72df67f1599d346e37936290f6184
Anatoly Ivakin <siggnull@gmail.com>
2024-04-17 16:55:54 +0300

    Implement the ConnectButton component

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,9 +1,33 @@
 import Button from '@mui/material/Button';
+import { useConnectWallet } from '@web3-onboard/react'
 
 function ConnectButton() {
-  return (
-    <Button variant="outlined" color="inherit">Connect Wallet</Button>
-  )
+    const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
+
+    function getButtonText() {
+        if (connecting) {
+            return 'Connecting...'
+        }
+
+        if (wallet) {
+            return 'Disconnect Wallet'
+        }
+
+        return 'Connect Wallet'
+    }
+
+    function onClick() {
+        if (wallet) {
+            disconnect(wallet)
+        } else {
+            connect()
+        }
+    }
+
+
+    return (
+        <Button variant="outlined" color="inherit" onClick={onClick}>{getButtonText()}</Button>
+    )
 }
-  
+
 export default ConnectButton
