8d68a7bf306ad6531a4708cf2723740bca291b0b
Anatoly Ivakin <siggnull@gmail.com>
2024-04-19 15:42:48 +0300

    Add a basic form for deposits and withdrawals

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import ConnectButton from './components/ConnectButton';
-import { init } from '@web3-onboard/react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import CssBaseline from '@mui/material/CssBaseline'
+import TextField from '@mui/material/TextField'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import { init, useConnectWallet } from '@web3-onboard/react'
 import initInjectedWallets from '@web3-onboard/injected-wallets'
+import ConnectButton from './components/ConnectButton'
 import './App.css'
 
 init({
@@ -26,8 +29,11 @@ init({
 })
 
 function App() {
+  const [{ wallet }] = useConnectWallet()
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <>
+      <CssBaseline/>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
@@ -36,7 +42,33 @@ function App() {
           <ConnectButton/>
         </Toolbar>
       </AppBar>
-    </Box>
+      <Box component="form"
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+        visibility={wallet ? 'visible' : 'hidden'}
+      >
+        <Box>
+          <Box>
+            <Box>Wallet: {wallet?.accounts[0].address}</Box>
+          </Box>
+          <Box>
+            <Box>Balance: 0 mETH</Box>
+          </Box>
+          <Box>
+            <TextField label="Amount" size="small"></TextField>
+            <Button variant="contained" style={{ width: 120}}>Deposit</Button>
+          </Box>
+          <Box>
+            <TextField label="Amount" size="small"></TextField>
+            <Button variant="contained" style={{ width: 120}}>Withthdraw</Button>
+          </Box>
+        </Box>
+      </Box>
+    </>
   )
 }
 
