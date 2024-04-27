91673c1b8537645e8113543ede9c22f7ebe98726
Anatoly Ivakin <siggnull@gmail.com>
2024-04-27 20:59:01 +0300

    Add SimpleToken initialization tests

--- blockchain/test/SimpleToken.ts
+++ blockchain/test/SimpleToken.ts
@@ -9,9 +9,12 @@ describe("SimpleToken", function () {
         const SimpleToken = await hre.ethers.getContractFactory("SimpleToken")
         const simpleToken = await SimpleToken.deploy()
 
-        const [owner, otherSigner] = await hre.ethers.getSigners();
+        const SimplePool = await hre.ethers.getContractFactory("SimplePool")
+        const simplePool = await SimplePool.deploy()
 
-        return { simpleToken, owner, otherSigner }
+        const [owner, alice] = await hre.ethers.getSigners();
+
+        return { simpleToken, simplePool, owner, alice }
     }
 
     describe("Deployment", function () {
@@ -43,10 +46,33 @@ describe("SimpleToken", function () {
             expect(await simpleToken.totalSupply()).to.equal(0)
         })
 
-        it ("Should have zero total shares", async function () {
+        it("Should have zero total shares", async function () {
             const { simpleToken } = await loadFixture(deploySimpleTokenFixture)
 
             expect(await simpleToken.totalShares()).to.equal(0)
         })
+    })
+
+    describe("Initialization", function () {
+        it("Should be reverted if initialized by a non-owner", async function () {
+            const { simpleToken, simplePool, alice } = await loadFixture(deploySimpleTokenFixture)
+
+            await expect(simpleToken.connect(alice).initialize(simplePool)).to.be.revertedWithCustomError(simpleToken, ("OwnableUnauthorizedAccount"))
+        })
+
+        it("Should not be reverted if initialized by the owner", async function () {
+            const { simpleToken, simplePool } = await loadFixture(deploySimpleTokenFixture)
+
+            await expect(simpleToken.initialize(simplePool)).to.not.be.reverted
+        })
+
+        // it ("Should not fail when calling mint as pool", async function () {
+        //     const { simpleToken, simplePool, alice } = await loadFixture(deploySimpleTokenFixture)
+
+        //     const simplePoolSigner = await hre.ethers.getImpersonatedSigner(await simplePool.getAddress())
+
+        //     await expect(simpleToken.initialize(simplePool)).to.not.be.reverted
+        //     await expect(simpleToken.connect(simplePoolSigner).mint(alice.address, 1)).to.not.be.reverted
+        // })
     })
 })
