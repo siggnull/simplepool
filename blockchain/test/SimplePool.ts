5712177a12236a26042a120bcd584a2b53078fc1
Anatoly Ivakin <siggnull@gmail.com>
2024-04-29 10:39:17 +0300

    Replace ethereum amount constants with a shorthand function

--- blockchain/test/SimplePool.ts
+++ blockchain/test/SimplePool.ts
@@ -3,12 +3,15 @@ import { expect } from "chai"
 import hre from "hardhat"
 
 
-describe("SimplePool", function () {
-    const ONE_TENTH_ETHER = hre.ethers.parseEther("0.1")
-    const ONE_ETHER = hre.ethers.parseEther("1")
-    const TWO_ETHER = hre.ethers.parseEther("2")
-    const THREE_ETHER = hre.ethers.parseEther("3")
+function eth(value: number | string) {
+    if (typeof value === "number") {
+        value = value.toString()
+    }
+    return hre.ethers.parseEther(value)
+}
 
+
+describe("SimplePool", function () {
     async function deployPoolFixture() {
         const SimplePool = await hre.ethers.getContractFactory("SimplePool")
         const simplePool = await SimplePool.deploy()
@@ -52,29 +55,29 @@ describe("SimplePool", function () {
         it("Should fail when deposit is called", async function () {
             const { simplePool } = await loadFixture(deployPoolFixture)
 
-            await expect(simplePool.deposit({ value: ONE_ETHER })).to.be.revertedWithCustomError(simplePool, ("NotInitialized"))
+            await expect(simplePool.deposit({ value: eth(1) })).to.be.revertedWithCustomError(simplePool, ("NotInitialized"))
         })
 
         it("Should fail when withdraw is called", async function () {
             const { simplePool } = await loadFixture(deployPoolFixture)
 
-            await expect(simplePool.withdraw(ONE_ETHER)).to.be.revertedWithCustomError(simplePool, ("NotInitialized"))
+            await expect(simplePool.withdraw(eth(1))).to.be.revertedWithCustomError(simplePool, ("NotInitialized"))
         })
 
         it("Should succeed when reward is called and return the correct amount when calling totalSupply", async function () {
             const { simplePool } = await loadFixture(deployPoolFixture)
 
-            await expect(simplePool.reward({ value: ONE_ETHER })).to.not.be.reverted
-            expect(await simplePool.totalSupply()).to.equal(ONE_ETHER)
+            await expect(simplePool.reward({ value: eth(1) })).to.not.be.reverted
+            expect(await simplePool.totalSupply()).to.equal(eth(1))
         })
 
         it("Should accept incoming transfers and return the correct amount when calling totalSupply", async function () {
             const { simplePool, alice, bob } = await loadFixture(deployPoolFixture)
             
-            await expect(alice.sendTransaction({ to: simplePool, value: ONE_ETHER })).to.not.be.reverted
-            expect(await simplePool.totalSupply()).to.equal(ONE_ETHER)
-            await expect(bob.sendTransaction({ to: simplePool, value: TWO_ETHER })).to.not.be.reverted
-            expect(await simplePool.totalSupply()).to.equal(THREE_ETHER)
+            await expect(alice.sendTransaction({ to: simplePool, value: eth(1) })).to.not.be.reverted
+            expect(await simplePool.totalSupply()).to.equal(eth(1))
+            await expect(bob.sendTransaction({ to: simplePool, value: eth(2) })).to.not.be.reverted
+            expect(await simplePool.totalSupply()).to.equal(eth(3))
         })
     })
 
@@ -96,31 +99,31 @@ describe("SimplePool", function () {
         it("Should succeed when deposit is called", async function () {
             const { simplePool, alice } = await loadFixture(initializePoolFixture)
 
-            await expect(simplePool.connect(alice).deposit({ value: ONE_ETHER })).to.not.be.reverted
+            await expect(simplePool.connect(alice).deposit({ value: eth(1) })).to.not.be.reverted
         })
 
         it("Should fail when withdrawal amount is too high", async function () {
             const { simplePool, alice } = await loadFixture(initializePoolFixture)
 
-            await expect(simplePool.connect(alice).deposit({ value: ONE_ETHER })).to.not.be.reverted
-            await expect(simplePool.connect(alice).withdraw(TWO_ETHER)).to.be.revertedWithCustomError(simplePool, ("InsufficientLiquidity"))
+            await expect(simplePool.connect(alice).deposit({ value: eth(1) })).to.not.be.reverted
+            await expect(simplePool.connect(alice).withdraw(eth(2))).to.be.revertedWithCustomError(simplePool, ("InsufficientLiquidity"))
         })
 
         it("Should succeed when withdrawal amount less or equal to the amount deposited", async function () {
             const { simplePool, alice } = await loadFixture(initializePoolFixture)
 
-            await expect(simplePool.connect(alice).deposit({ value: TWO_ETHER })).to.not.be.reverted
-            await expect(simplePool.connect(alice).withdraw(ONE_ETHER)).to.not.be.reverted
-            await expect(simplePool.connect(alice).withdraw(ONE_ETHER)).to.not.be.reverted
+            await expect(simplePool.connect(alice).deposit({ value: eth(2) })).to.not.be.reverted
+            await expect(simplePool.connect(alice).withdraw(eth(1))).to.not.be.reverted
+            await expect(simplePool.connect(alice).withdraw(eth(1))).to.not.be.reverted
         })
 
         it("Should let the user withdraw the entire pooled amount with rewards if he is the only shareholder", async function () {
             const { simplePool, alice } = await loadFixture(initializePoolFixture)
 
-            await expect(simplePool.connect(alice).deposit({ value: ONE_ETHER })).to.not.be.reverted
-            await expect(simplePool.reward({ value: ONE_TENTH_ETHER })).to.not.be.reverted
+            await expect(simplePool.connect(alice).deposit({ value: eth(1) })).to.not.be.reverted
+            await expect(simplePool.reward({ value: eth(0.1) })).to.not.be.reverted
 
-            const expectedAmount = hre.ethers.parseEther("1.1")
+            const expectedAmount = eth(1.1)
             expect(await simplePool.totalSupply()).to.equal(expectedAmount)
             expect(await simplePool.balanceOf(alice.address)).to.equal(expectedAmount)
             await expect(simplePool.connect(alice).withdraw(expectedAmount)).to.not.be.reverted
