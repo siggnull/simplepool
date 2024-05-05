d56f6c79d7713158cda0cc0e5be84f692347b172
Anatoly Ivakin <siggnull@gmail.com>
2024-05-05 13:11:58 +0300

    Unite ignition scripts

--- blockchain/ignition/modules/SimplePool.ts
+++ blockchain/ignition/modules/SimplePool.ts
@@ -2,8 +2,12 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const SimplePoolModule = buildModule("SimplePoolModule", (m) => {
   const simplePool = m.contract("SimplePool");
+  const simpleToken = m.contract("SimpleToken");
 
-  return { simplePool };
+  m.call(simplePool, "initialize", [simpleToken]);
+  m.call(simpleToken, "initialize", [simplePool]);
+
+  return { simplePool, simpleToken };
 });
 
 export default SimplePoolModule;
--- blockchain/ignition/modules/SimpleToken.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const SimpleTokenModule = buildModule("SimpleTokenModule", (m) => {
-  const simpleToken = m.contract("SimpleToken");
-
-  return { simpleToken };
-});
-
-export default SimpleTokenModule;
