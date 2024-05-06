eac98ba25e835a3692ab229753efd469bd032c61
Anatoly Ivakin <siggnull@gmail.com>
2024-05-06 19:30:30 +0300

    Save generated typechain files in the frontend directory

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -3,6 +3,10 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  typechain: {
+    outDir: "../frontend/typechain",
+    target: "ethers-v6",
+  },
 };
 
 export default config;
