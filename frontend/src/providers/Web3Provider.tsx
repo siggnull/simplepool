24cdc36d4c9173344a13f9062103fbf1e80613af
Anatoly Ivakin <siggnull@gmail.com>
2024-05-14 19:36:51 +0300

    Add the Hardhat network

--- frontend/src/providers/Web3Provider.tsx
+++ frontend/src/providers/Web3Provider.tsx
@@ -1,13 +1,13 @@
 import { ReactNode } from "react"
 import { WagmiProvider, createConfig } from "wagmi"
-import { sepolia } from "wagmi/chains"
+import { sepolia, hardhat } from "wagmi/chains"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ConnectKitProvider, getDefaultConfig } from "connectkit"
 
 const config = createConfig(
   getDefaultConfig({
     appName: "Simple Pool",
-    chains: [sepolia],
+    chains: [sepolia, hardhat],
     walletConnectProjectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID,
   }),
 )
